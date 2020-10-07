d2520e57ebc8e585c3873f1ccbcbbcf870ec83d2
JoaoLucasXavier <lucasxavier000@gmail.com>
2020-10-07 00:16:32 -0300

    Add interfaces

---
+++ src/sfm.Domain/Interfaces/Category/ICategory.cs
@@ -0,0 +1,10 @@
+using sfm.Domain.Interfaces.Generics;
+using sfm.Entities.Entities;
+
+namespace sfm.Domain.Interfaces.Category
+{
+    public interface ICategory : IGeneric<Category>
+    {
+
+    }
+}
